a1f2d5fc040443623fd956a79d4286f2b98a29f4
Elaine Broussard <elaine@elainebroussard.com>
2016-04-16 01:03:09 -0500

    add custom jquery code

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
 	$('.expander').simpleexpand();
 	$("#photocontainer").twentytwenty();
+	$('.expander').hover(
+		function() {
+			$( this ).css( "color", "#ad4863" ).css( "font-weight", "bold" );
+		}, function() {
+			$( this ).css( "color", "#1B515E" ).css( "font-weight", "normal" );
+		}
+	);
 });
